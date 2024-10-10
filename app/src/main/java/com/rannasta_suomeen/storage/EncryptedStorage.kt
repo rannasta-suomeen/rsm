5140b241ec09abc9e155a7b901aef9fab48306b7
Jere Hasu <31950088+zexee24@users.noreply.github.com>
2024-10-10 11:16:10 +0300

    fix: Crash when creating EncryptedStorage.kt

--- app/src/main/java/com/rannasta_suomeen/storage/EncryptedStorage.kt
+++ app/src/main/java/com/rannasta_suomeen/storage/EncryptedStorage.kt
@@ -1,18 +1,32 @@
 package com.rannasta_suomeen.storage
 
 import android.content.Context
+import android.util.Log
 import androidx.security.crypto.EncryptedSharedPreferences
 import androidx.security.crypto.MasterKey
 
 class EncryptedStorage(context: Context) {
     private val fileName = "com.rannasta-suomeen.rsm.encrypted"
     private val key = MasterKey.Builder(context).setKeyScheme(MasterKey.KeyScheme.AES256_GCM).build()
-    private val preferences = EncryptedSharedPreferences(
+    private val preferences = try {
+        EncryptedSharedPreferences(
         context,
         fileName,
         key,
         EncryptedSharedPreferences.PrefKeyEncryptionScheme.AES256_SIV,
         EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM)
+    } catch (e: RuntimeException){
+        Log.d("Storage", "Creating encryptedPreferences failed, deleting old file and trying anew")
+        context.filesDir.parentFile?.listFiles()?.find {
+            it.name=="shared_prefs"
+        }?.listFiles()?.find { it.name == fileName }?.delete()
+        EncryptedSharedPreferences(
+            context,
+            fileName,
+            key,
+            EncryptedSharedPreferences.PrefKeyEncryptionScheme.AES256_SIV,
+            EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM)
+    }
 
     var userName
     get() = preferences.getString("userName", null)
